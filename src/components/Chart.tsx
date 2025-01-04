3ddf7156cbb55fd900e97ad4aac77dc46a2cd465
adetayo adewobi <adewobiadetayo1@gmail.com>
2025-01-04 22:36:27 +0100

    feat: chart API integration

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -9,6 +9,8 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import api from "../utils/apiHelper";
+import { toast } from "react-toastify";
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -19,18 +21,44 @@ ChartJS.register(
 );
 
 export const AnalyticsChart: React.FC = () => {
-  const data = {
-    labels: ["Engineering", "HR", "Finance", "Marketing"],
-    datasets: [
-      {
-        label: "Headcount by Department",
-        data: [10, 5, 7, 3], // Example headcount data
-        backgroundColor: "rgba(54, 162, 235, 0.5)",
-        borderColor: "rgba(54, 162, 235, 1)",
-        borderWidth: 1,
-      },
-    ],
-  };
+  const [chartData, setChartData] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchChartData = async () => {
+      try {
+        const response = await api.get("dashboard/employees/count");
+        const data = response.data.data;
+        console.log(response);
+
+        const labels = data.employeesPerDepartment.map(
+          (item: { department: string }) => item.department
+        );
+        const counts = data.employeesPerDepartment.map(
+          (item: { count: number }) => item.count
+        );
+
+        setChartData({
+          labels: labels,
+          datasets: [
+            {
+              label: "Headcount by Department",
+              data: counts,
+              backgroundColor: "rgba(54, 162, 235, 0.5)",
+              borderColor: "rgba(54, 162, 235, 1)",
+              borderWidth: 1,
+            },
+          ],
+        });
+      } catch (error: any) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchChartData();
+  }, []);
 
   const options = {
     responsive: true,
@@ -45,9 +73,17 @@ export const AnalyticsChart: React.FC = () => {
     },
   };
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="w-full h-[15rem] sm:h-[20rem]">
-      <Bar data={data} options={options} />
+    <div className="w-[80vw] h-[15rem] sm:h-[20rem]">
+      {chartData ? (
+        <Bar data={chartData} options={options} />
+      ) : (
+        <div>No data available</div>
+      )}
     </div>
   );
 };
